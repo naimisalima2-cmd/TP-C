18d7f1b8500c7447e3620ec1346d804343acd5a9
naimisalima2-cmd <naimisalima2@gmail.com>
2025-10-14 11:32:14 +0100

    Ajout de la méthode saisie dans la classe Heure

--- TP4/Heure.cpp
+++ TP4/Heure.cpp
@@ -2,27 +2,36 @@
 #include <iostream>
 using namespace std;
 
-bool Heure::checkHeure(int h, int m) {
-    return (h >= 0 && h <= 23) && (m >= 0 && m <= 59);
+Heure::Heure() {
+    heures = 0;
+    minutes = 0;
 }
 
 Heure::Heure(int h, int m) {
-    if (checkHeure(h, m)) {
-        heures = h; minutes = m;
-    } else {
-        heures = 0; minutes = 0;
-    }
+    if (h >= 0 && h <= 23) heures = h; else heures = 0;
+    if (m >= 0 && m <= 59) minutes = m; else minutes = 0;
+}
+
+void Heure::saisie() {
+    int h, m;
+    do {
+        cout << "Entrez l'heure (0-23) : "; cin >> h;
+    } while (h < 0 || h > 23);
+
+    do {
+        cout << "Entrez les minutes (0-59) : "; cin >> m;
+    } while (m < 0 || m > 59);
+
+    heures = h;
+    minutes = m;
+}
+
+void Heure::affiche() const {
+    cout << (heures < 10 ? "0" : "") << heures << ":"
+         << (minutes < 10 ? "0" : "") << minutes;
 }
 
 int Heure::getHeures() const { return heures; }
 int Heure::getMinutes() const { return minutes; }
-
-void Heure::setHeures(int h) { if (checkHeure(h, minutes)) heures = h; }
-void Heure::setMinutes(int m) { if (checkHeure(heures, m)) minutes = m; }
-
-void Heure::affiche() const {
-    cout << (heures < 10 ? "0" : "") << heures << ":"
-         << (minutes < 10 ? "0" : "") << minutes << endl;
-}
-
-
+void Heure::setHeures(int h) { if (h >= 0 && h <= 23) heures = h; }
+void Heure::setMinutes(int m) { if (m >= 0 && m <= 59) minutes = m; }
--- TP4/Heure.h
+++ TP4/Heure.h
@@ -1,21 +1,23 @@
 #ifndef HEURE_H
 #define HEURE_H
 
+#include <iostream>
+using namespace std;
+
 class Heure {
 private:
-    int heures, minutes;
-    bool checkHeure(int h, int m);
+    int heures;
+    int minutes;
 
 public:
-    Heure(int h = 0, int m = 0);
-
+    Heure();
+    Heure(int h, int m);
+    void saisie();
+    void affiche() const;
     int getHeures() const;
     int getMinutes() const;
-
     void setHeures(int h);
     void setMinutes(int m);
-
-    void affiche() const;
 };
 
 #endif
