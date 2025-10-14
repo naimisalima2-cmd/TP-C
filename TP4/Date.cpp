6c2e5ab6e90f4882651d452fbfc848d2f3a59622
naimisalima2-cmd <naimisalima2@gmail.com>
2025-10-14 11:31:23 +0100

    Ajout de la méthode saisie

--- TP4/Date.cpp
+++ TP4/Date.cpp
@@ -2,46 +2,58 @@
 #include <iostream>
 using namespace std;
 
-
-bool Date::checkDate(int j, int m, int a){
-    if (j < 1 || j> 31) {
-        return false;
-    }
-    else if (m < 1 || m >12){
-        return false;
-    }
-    else if (a < 2000 || a >2050){
-        return false;    }
-    else return true;
+Date::Date() {
+    jour = 1;
+    mois = 1;
+    annee = 2000;
 }
 
-
 Date::Date(int j, int m, int a) {
-    if (checkDate(j, m, a)==true) {
+    if (checkDate(j, m, a)) {
         jour = j;
         mois = m;
         annee = a;
-    }
-    else {
+    } else {
         jour = 1;
         mois = 1;
         annee = 2000;
     }
 }
-int Date::getJour() const {
-     return jour; }
-int Date::getMois() const {
-    return mois;}
-int Date::getAnnee() const {
-    return annee;}
-void Date::setJour(int j) { 
-    if (checkDate(jour, mois, annee)) jour = j; }
-void Date::setMois(int m) {
-    if (checkDate(jour, mois, annee)) mois=m;}
-void Date::setAnnee(int a) {
-    if (checkDate(jour, mois, annee)) annee=a;}
 
-void Date::affiche() const {
-    cout << jour << "/" << mois << "/" << annee << endl ;
+void Date::saisie() {
+    int j, m, a;
+    do {
+        cout << "Entrez le jour : "; cin >> j;
+    } while (!checkDate(j, 1, 2000));
+
+    do {
+        cout << "Entrez le mois : "; cin >> m;
+    } while (m < 1 || m > 12);
+
+    do {
+        cout << "Entrez l'annee : "; cin >> a;
+    } while (a < 2000 || a > 2050);
+
+    jour = j;
+    mois = m;
+    annee = a;
 }
 
+bool Date::checkDate(int j, int m, int a) const {
+    if (j < 1 || j > 31) return false;
+    if (m < 1 || m > 12) return false;
+    if (a < 2000 || a > 2050) return false;
+    return true;
+}
+
+int Date::getJour() const { return jour; }
+int Date::getMois() const { return mois; }
+int Date::getAnnee() const { return annee; }
+
+void Date::setJour(int j) { if (checkDate(j, mois, annee)) jour = j; }
+void Date::setMois(int m) { if (checkDate(jour, m, annee)) mois = m; }
+void Date::setAnnee(int a) { if (checkDate(jour, mois, a)) annee = a; }
+
+void Date::affiche() const {
+    cout << jour << "/" << mois << "/" << annee;
+}
--- TP4/Date.h
+++ TP4/Date.h
@@ -1,18 +1,27 @@
 #ifndef DATE_H
 #define DATE_H
+
+#include <iostream>
+using namespace std;
+
 class Date {
 private:
-    int jour, mois, annee;
-    bool checkDate(int j, int m, int a);
+    int jour;
+    int mois;
+    int annee;
+
 public:
-    Date(int j = 1, int m = 1, int a = 2000);
+    Date();
+    Date(int j, int m, int a);
+    void saisie();
+    void affiche() const;
+    bool checkDate(int j, int m, int a) const;
     int getJour() const;
     int getMois() const;
     int getAnnee() const;
     void setJour(int j);
     void setMois(int m);
     void setAnnee(int a);
-    void affiche() const;
-    
 };
+
 #endif
